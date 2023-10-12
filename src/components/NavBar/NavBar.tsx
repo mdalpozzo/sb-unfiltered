c2088362e3f4cfea0402f94de88c3a6dc13ab631
marlin <marlin@cabana.life>
2023-10-12 14:17:20 +0900

    adjust linting. add mobile desktop tailwind utilities. show menu on mobile view in navbar.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { DarkModeToggle } from './DarkModeToggle'
 import { LogoSVG } from '@/images/logo/LogoSVG'
 import { cn } from '@/utils/cn'
+import { NavMenu } from './NavMenu'
 
 interface NavBarProps {
   initialTheme?: string
@@ -12,7 +13,8 @@ export function NavBar({ initialTheme }: NavBarProps) {
   return (
     <header
       className={cn([
-        `bg-inherit fixed top-0 left-0 right-0 z-50 border-b border-solid border-light-line`,
+        'h-16',
+        `bg-inherit fixed top-0 left-0 right-0 z-50 border-b border-solid border-light-lin`,
         // dark mode
         `dark:border-dark-line dark:bg-zinc-900`,
         // shadow
@@ -20,7 +22,7 @@ export function NavBar({ initialTheme }: NavBarProps) {
       ])}
     >
       <nav
-        className="relative flex items-center justify-center p-1 lg:px-8"
+        className="relative h-full w-full flex items-center justify-center p-1 lg:px-8"
         aria-label="Global"
       >
         <Link
@@ -28,16 +30,25 @@ export function NavBar({ initialTheme }: NavBarProps) {
           aria-label="Santa Barbara Unfiltered"
           className="flex justify-center flex-grow"
         >
-          <div className="relative">
+          <div className="relative flex justify-center items-center">
             <LogoSVG
               className={cn(`fill-dark-bg dark:fill-light-bg`)}
-              width={250}
+              width="auto"
+              height="75%"
             />
           </div>
         </Link>
 
-        <div className="absolute right-4 top-1/2 transform -translate-y-1/2">
-          <DarkModeToggle initialTheme={initialTheme} />
+        <div className="absolute right-4">
+          <div className="desktop">
+            <DarkModeToggle initialTheme={initialTheme} />
+          </div>
+        </div>
+
+        <div className="absolute right-1">
+          <div className="mobile">
+            <NavMenu />
+          </div>
         </div>
       </nav>
     </header>

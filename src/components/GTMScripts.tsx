a6dde21763681f5fae01526777856ea14f9b2e8f
marlin <marlin@cabana.life>
2023-10-27 23:32:37 -0700

    fix ga gtm.

--- src/components/GTMScripts.tsx
+++ src/components/GTMScripts.tsx
@@ -4,11 +4,11 @@ export function GTMScript() {
   return (
     <>
       <Script id="gtm" strategy="afterInteractive">{`
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T3WS8858')
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PWL3M2W3')
       `}</Script>
     </>
   )
@@ -19,8 +19,8 @@ export function GTMNoScript() {
     <noscript
       dangerouslySetInnerHTML={{
         __html: `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3WS8858"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWL3M2W3"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
       `,
       }}
     />
